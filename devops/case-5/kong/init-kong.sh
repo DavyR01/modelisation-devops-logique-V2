52196950983453800f7e887a86230ab18c42f654
Davy <100140271+DavyR01@users.noreply.github.com>
2026-04-14 11:32:03 +0200

    case 5 - fix(kong): correct route bootstrap shell syntax

--- devops/case-5/kong/init-kong.sh
+++ devops/case-5/kong/init-kong.sh
@@ -18,16 +18,19 @@ curl -s -X PUT http://kong:8001/services/product-service \
 
 curl -s -X PUT http://kong:8001/services/order-service \
   --data url=http://order-service:80
-
+ 
 # Routes
 curl -s -X PUT http://kong:8001/services/user-service/routes/user-route \
-  --data paths[]=/api/users
+  --data paths[]=/api/users \
+  --data strip_path=false
 
 curl -s -X PUT http://kong:8001/services/product-service/routes/product-route \
-  --data paths[]=/api/products
+  --data paths[]=/api/products \
+  --data strip_path=false
 
 curl -s -X PUT http://kong:8001/services/order-service/routes/order-route \
-  --data paths[]=/api/orders
-
+  --data paths[]=/api/orders \
+  --data strip_path=false
+  
 echo
 echo "Kong configuration done."
